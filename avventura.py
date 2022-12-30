dcbf7c66557938bbe65d0cfbe74c990eb9e41a89
gbaromhunter <gianluca.toto92@gmail.com>
2022-12-30 11:52:05 +0200

    first commit

--- avventura.py
+++ avventura.py
@@ -13,9 +13,20 @@ Bisogna che crei una data structure apposita per i passaggi.
 Ogni passaggio come attributo avrà il testo, le parole chiave da poter approfondire, e un dizionario che punta al nome
 del passaggio successivo
 """
+from rich import print
 from rich.console import Console
+from rich.layout import Layout
+from rich.pretty import Pretty
 
 console = Console()
+
+
+def create_splitted_layout(upper_title, lower_title, ratio):
+    layout = Layout()
+    layout.split_column(Layout(name=upper_title), Layout(name=lower_title))
+    layout[upper_title].size = None
+    layout[upper_title].ratio = ratio
+    return layout
 
 
 def color_word(word, color):
@@ -24,22 +35,22 @@ def color_word(word, color):
 
 class Node:
 
-    def __init__(self, title="", text="", child=None, parent=None, highlighted=None):
+    def __init__(self, upper_title="", lower_title="", ratio=3, text="", child=None, parent=None, highlighted=None):
+        self.upper_title = upper_title
+        self.lower_title = lower_title
         if child is None:
             child = {}
         if parent is None:
             parent = {}
-        self.title = title
         self.text = text
         self.parent = parent
         self.child = child
-        if highlighted is None:
-            highlighted = {}
-        else:
-            highlighted = {col: color_word(col, highlighted[col]) for col in highlighted}
-
-    def get_title(self):
-        return self.title
+        self.layout = create_splitted_layout(self.upper_title, self.lower_title, ratio)
+        self.actions = list(highlighted)
+        self.highlighted = highlighted
+        if highlighted:
+            for key in highlighted:
+                self.text = self.text.replace(key, color_word(key, highlighted[key]))
 
     def get_text(self):
         return self.text
@@ -50,8 +61,8 @@ class Node:
     def get_child(self):
         return self.child
 
-    def set_title(self, new_title):
-        self.title = new_title
+    def set_highlighted(self, highlighted):
+        self.highlighted = highlighted
 
     def set_text(self, new_text):
         self.text = new_text
@@ -71,7 +82,15 @@ class Node:
                 self.parent[key] = parent
 
     def display(self):
-        console.rule(f"[bold red]{self.title}")
+        actions = Pretty(self.actions)
+        self.layout[self.upper_title].update(self.text)
+        self.layout[self.lower_title].update(actions)
+        print(self.layout)
 
 
-inizio = Node("Nodo di prova", "Questo è un nodo di prova per vedere come esce")
+inizio = Node(upper_title="Nodo di prova",
+              lower_title="Azioni",
+              ratio=3,
+              text="Questo è un nodo di prova per vedere come esce",
+              highlighted={"prova": "green"})
+inizio.display()
